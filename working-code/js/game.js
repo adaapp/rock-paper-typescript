cbdc63522560ed87852a6f6b99f643e6d1e59ced
Alasdair Blackwell <aliblackwell@protonmail.com>
2019-08-18 20:55:25 +0100

    Start basic game

--- working-code/js/game.js
+++ working-code/js/game.js
@@ -1,5 +1,10 @@
 alert("Hello, world! Love, TypeScript");
 var userScore = 0;
-var gameScore = 0;
+var computerScore = 0;
 var counter = 0;
+function startGame() {
+    counter = 0;
+    userScore = 0;
+    computerScore = 0;
+}
 //# sourceMappingURL=game.js.map
