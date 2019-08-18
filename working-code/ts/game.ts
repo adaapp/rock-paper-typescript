7a95e42d95e44934fe08ab5ad0b3e3cf1d0e337b
Alasdair Blackwell <aliblackwell@protonmail.com>
2019-08-18 20:55:25 +0100

    Start basic game

--- working-code/ts/game.ts
+++ working-code/ts/game.ts
@@ -2,3 +2,9 @@ alert("Hello, world! Love, TypeScript")
 let userScore: number = 0
 let computerScore: number = 0
 let counter: number = 0
+
+function startGame(): void {
+  counter = 0
+  userScore = 0
+  computerScore = 0
+}
