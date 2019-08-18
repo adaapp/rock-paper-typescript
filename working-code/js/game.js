a22f310ab76f44a76fddd57cc5a868a172db18c3
Alasdair Blackwell <aliblackwell@protonmail.com>
2019-08-18 22:55:34 +0100

    Added counter and score

--- working-code/js/game.js
+++ working-code/js/game.js
@@ -1,6 +1,9 @@
 var userScore = 0;
 var computerScore = 0;
 var counter = 0;
+var startButton = document.querySelector(".start-button");
+var gameButtons = document.querySelector(".game-buttons");
+var output = document.querySelector(".output");
 var moves;
 (function (moves) {
     moves[moves["Rock"] = 0] = "Rock";
@@ -11,18 +14,23 @@ function startGame() {
     userScore = 0;
     computerScore = 0;
     counter = 0;
-    outputMessage('The game has begun!');
+    clearOutput();
+    outputMessage("The game has begun!");
     hideStartButton();
 }
+function clearOutput() {
+    output.innerHTML = '';
+}
 function outputMessage(message) {
-    var output = document.querySelector('.output');
     output.innerHTML += "<p>" + message + "</p>";
 }
 function hideStartButton() {
-    var startButton = document.querySelector(".start-button");
-    var gameButtons = document.querySelector(".game-buttons");
     startButton.style.display = "none";
     gameButtons.style.display = "block";
+}
+function showStartButton() {
+    startButton.style.display = "inline-block";
+    gameButtons.style.display = "none";
 }
 function getComputerMove() {
     var move = Math.floor(Math.random() * 3);
@@ -37,14 +45,26 @@ function handleUserChoice(choice) {
         player: "User"
     };
     var computerGuess = getComputerMove();
-    outputMessage("You chose " + moves[userGuess.move]);
-    outputMessage("Computer chose " + moves[computerGuess.move]);
     var winner = calculateWinner(userGuess, computerGuess);
     if (winner.player === "User")
         userScore++;
     if (winner.player === "Computer")
         computerScore++;
-    outputMessage(winner.player + " wins with " + winner.move);
+    outputMessage(winner.player + " wins with " + moves[winner.move]);
+    checkRoundProgress();
+}
+function checkRoundProgress() {
+    counter++;
+    if (userScore === 2 || computerScore === 2 || counter === 3) {
+        showStartButton();
+        counter = 0;
+        if (userScore > computerScore)
+            outputMessage("You won that round!");
+        else if (userScore === computerScore)
+            outputMessage("You drew that round");
+        else
+            outputMessage("You lost that round!");
+    }
 }
 function calculateWinner(guessOne, guessTwo) {
     if (guessOne.move == guessTwo.move)
